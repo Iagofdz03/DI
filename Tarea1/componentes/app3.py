c042067e09bba1cb0269e7dd298c34507a00dcc2
Iagofdz03 <iagofzdz@gmail.com>
2025-11-14 09:29:40 +0100

    Create app3.py

---
+++ Tarea1/componentes/app3.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import QApplication, QMainWindow
+from componentes.etiqueta_saludo import EtiquetaSaludo
+
+app = QApplication([])
+win = QMainWindow()
+win.setWindowTitle("Reutilizando un componente")
+win.setCentralWidget(EtiquetaSaludo("Hola desde mi componente"))
+win.show()
+app.exec()
